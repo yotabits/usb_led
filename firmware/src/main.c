f631f20e595bd529cda3b595c6e2297ad5af7cc2
Thomas Kostas <tkostas75@gmail.com>
2019-12-05 10:23:06 +0900

    Firmware: Add rgb led support
    RGB PIN is set as an output.
    3 bytes data sent to device with bRequest = 2 is treated as rgb data to be
    displayed by the RGB LED.
    RGB led (PB2) and onboard led (PB1) are both updated every time new
    data is received by the device.

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -4,8 +4,14 @@
 #include <util/delay.h>
 
 #include "usbdrv.h"
+#include "WS2811.h"
 
-#define LED     PB1
+
+#define LED             PB1
+#define RGB_LED         PB2
+#define RGB_LED_NUMBER  1
+
+DEFINE_WS2811_FN(WS2811RGB, PORTB, RGB_LED)
 
 #if F_CPU != 16500000L
   #error "Wrong F_CPU"
@@ -23,6 +29,13 @@ PROGMEM const char usbHidReportDescriptor[22] = {   /* USB report descriptor */
   0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
   0xc0                           // END_COLLECTION
 };
+
+typedef struct leds_status
+{
+    RGB_t rgb_led[RGB_LED_NUMBER];
+    uint8_t led_status;
+} leds_status_t;
+leds_status_t led_status_g = {{{0}}};
 
 void set_led_to(uchar value)
 {
@@ -54,14 +67,27 @@ usbMsgLen_t usbFunctionSetup(uchar setupData[8])
 
 uchar usbFunctionWrite(uchar *data, uchar len)
 {
-    set_led_to(data[0]);
+    //set_led_to(data[0]);
+    if (len == 1)
+    {
+        led_status_g.led_status = data[0];
+    }
+    else if (len == 3)
+    {
+        led_status_g.rgb_led[0].r = data[0];
+        led_status_g.rgb_led[0].g = data[1];
+        led_status_g.rgb_led[0].b = data[2];
+    }
+    update_leds(led_status_g);
     return 1;
 }
 
 void setup_led_as_output()
 {
     DDRB |= (1 << LED); // set led pin as output
-    set_led_to(0); // Turn on led
+    set_led_to(0); // Turn off led
+
+    DDRB |= (1 << RGB_LED); // set rgb led pin as output
 }
 
 
@@ -80,14 +106,19 @@ void begin()
   usbDeviceConnect();
   sei();
 }
-
+void update_leds(leds_status_t status)
+{
+    WS2811RGB(status.rgb_led, ARRAYLEN(status.rgb_led));
+    set_led_to(status.led_status);
+}
 int main()
 {
+    //RGB_t rgb[1];
     setup_led_as_output();
     begin();
-  while(1)
-  {
+    while(1)
+    {
       usbPoll();
-  }
+    }
   return 0;
 }
